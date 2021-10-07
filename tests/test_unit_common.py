92497361061fc98fc5bc15a77a2a47c3d34dfd29
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2021-10-07 14:26:11 +0200

    fix import error

--- tests/test_unit_common.py
+++ tests/test_unit_common.py
@@ -3,7 +3,7 @@ import tempfile
 import unittest
 from pathlib import Path
 
-from album import AlbumRunner, push_active_solution
+from album.runner import AlbumRunner, push_active_solution
 
 
 class TestUnitCommon(unittest.TestCase):
