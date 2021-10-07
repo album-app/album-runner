92c519973c1ecd95125e77ff11c7e37121541314
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2021-10-07 14:22:19 +0200

    fix import error

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 from pathlib import Path
 
-from album import AlbumRunner
+from album.runner import AlbumRunner
 from album.runner.api import download_if_not_exists, extract_tar, in_target_environment
 from tests.test_unit_common import TestUnitCommon
 
