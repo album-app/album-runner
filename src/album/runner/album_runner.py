6e0210b71f91ebc4a9c9a7ca45ac2018a1cfb13c
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2021-11-10 11:25:56 +0100

    review 1

--- src/album/runner/album_runner.py
+++ src/album/runner/album_runner.py
@@ -5,11 +5,9 @@ class AlbumRunner:
         'group', 'name', 'version', 'description', 'url', 'license',
         'min_album_version', 'tested_album_version', 'args',
         'init', 'run', 'install', 'uninstall', 'pre_test', 'test',
-        'author', 'author_email',
-        'long_description', 'git_repo', 'dependencies',
+        'author', 'author_email', 'git_repo', 'dependencies',
         'timestamp', 'format_version', 'authors', 'cite', 'tags',
-        'documentation', 'covers', 'sample_inputs',
-        'sample_outputs', 'doi', 'catalog', 'parent', 'steps', 'close', 'title'
+        'documentation', 'covers', 'doi', 'catalog', 'parent', 'steps', 'close', 'title'
     ]
 
     # CAUTION: deploy_keys also used for resolving. Make sure keys do not contain callable values.
@@ -31,12 +29,16 @@ class AlbumRunner:
     dependencies = None
     parent = None
     doi = None
+    title = None
+    description = None
+    license = None
+    git_repo = None
 
     args = []
     steps = []
     tags = []
     authors = []
-    cite = ""
+    cite = []
 
     # API keywords
     environment_path = None
--- src/album/runner/concept/script_creator.py
+++ src/album/runner/concept/script_creator.py
@@ -7,32 +7,19 @@ from album.runner.model.solution_script import SolutionScript
 class ScriptCreator(abc.ABC):
     """Abstract class for all ScriptCreator classes. Holds methods shared across all ScriptCreator classes."""
 
-    def __init__(self, execution_callback=None):
-        self.execution_block = None
-        self.common_inset = None
-        if execution_callback is not None and callable(execution_callback):
-            self.execution_callback = execution_callback
-        else:
-            self.reset_callback()
-
-    def reset_callback(self):
-        self.execution_callback = lambda: ""
-
-    @abc.abstractmethod
-    def set_execution_block(self, solution_object):
-        """The custom code for all scripts created by this class"""
+    def __init__(self):
         pass
 
     @abc.abstractmethod
-    def set_common_inset(self, solution_object):
+    def get_execution_block(self, solution_object) -> str:
+        """The custom code for all scripts created by this class"""
         pass
 
     def create_script(self, solution_object, argv) -> str:
         """Creates the script with the execution_block of the concrete instance of the class"""
-        self.set_common_inset(solution_object)
-        self.set_execution_block(solution_object)
+        execution_block = self.get_execution_block(solution_object)
 
-        script = SolutionScript(solution_object, self.execution_block, argv)
+        script = SolutionScript(solution_object, execution_block, argv)
 
         return script.create_solution_script()
 
@@ -41,41 +28,40 @@ class ScriptCreatorInstall(ScriptCreator):
     def __init__(self):
         super().__init__()
 
-    def set_execution_block(self, _):
-        self.execution_block = "\nget_active_solution().install()\n"
-
-    def set_common_inset(self, _):
-        self.common_inset = ""
+    def get_execution_block(self, _):
+        return "\nget_active_solution().install()\n"
 
 
 class ScriptCreatorUnInstall(ScriptCreator):
     def __init__(self):
         super().__init__()
 
-    def set_execution_block(self, _):
-        self.execution_block = "\nget_active_solution().uninstall()\n"
-
-    def set_common_inset(self, _):
-        self.common_inset = ""
+    def get_execution_block(self, _):
+        return "\nget_active_solution().uninstall()\n"
 
 
 class ScriptCreatorRun(ScriptCreator):
     def __init__(self, pop_solution: bool = False, execution_callback=None):
-        super().__init__(execution_callback)
+        super().__init__()
         self.pop_solution = pop_solution
 
-    def set_common_inset(self, _):
-        self.common_inset = ""
+        if execution_callback is not None and callable(execution_callback):
+            self.execution_callback = execution_callback
+        else:
+            self.reset_callback()
 
-    def set_execution_block(self, solution_object: AlbumRunner):
-        execution_block = self.common_inset
-        execution_block += "\nmodule_logger().info(\"Starting %s\")" % solution_object["name"]
+    def reset_callback(self):
+        self.execution_callback = lambda: ""
+
+    def get_execution_block(self, solution_object: AlbumRunner):
+        execution_block = "\nmodule_logger().info(\"Starting %s\")" % solution_object["name"]
         execution_block += "\nmodule_logger().info(\"\")\n"
         if solution_object['run'] and callable(solution_object['run']):
             execution_block += "\nget_active_solution().run()\n"
         else:
             raise ValueError("No \"run\" routine specified for solution \"%s\"! Aborting..." % solution_object["name"])
 
+        # used to insert code blocks during runtime after run but before close. Used when solution serves as parent.
         execution_block += self.execution_callback()
 
         if solution_object['close'] and callable(solution_object['close']):
@@ -87,23 +73,47 @@ class ScriptCreatorRun(ScriptCreator):
         if self.pop_solution:
             execution_block += "\npop_active_solution()\n"
 
-        self.execution_block = execution_block
+        return execution_block
+
+
+class ScriptCreatorRunWithParent(ScriptCreatorRun):
+    def __init__(self, parent_script_creator: ScriptCreatorRun, child_solution_list: list, child_args: list):
+        super().__init__()
+        self.parent_script_creator = parent_script_creator
+        self.child_solution_list = child_solution_list
+        self.child_args = child_args
+
+    def get_execution_block(self, solution_object) -> str:
+        self.parent_script_creator.execution_callback = self.create_child_scripts
+        execution_block = self.parent_script_creator.get_execution_block(solution_object)
+        self.parent_script_creator.reset_callback()
+
+        return execution_block
+
+    def create_child_scripts(self):
+        children_block = ""
+
+        for child_solution, child_arg in zip(self.child_solution_list, self.child_args):
+            s = ScriptCreatorRun(pop_solution=True)
+            child_script = s.create_script(child_solution, child_arg)
+
+            children_block += child_script
+        return children_block
 
 
 class ScriptTestCreator(ScriptCreatorRun):
     def __init__(self):
         super().__init__()
 
-    def set_common_inset(self, solution_object):
-        common_inset = "\nd = get_active_solution().pre_test()\n"
-        common_inset += "\nsys.argv = sys.argv + [\"=\".join([c, d[c]]) for c in d]\n"
+    def get_execution_block(self, solution_object):
+        execution_block = "\nd = get_active_solution().pre_test()\n"
+        execution_block += "\nsys.argv = sys.argv + [\"=\".join([c, d[c]]) for c in d]\n"
 
         # parse args again after pre_test() routine if necessary.
         if not solution_object["args"] == "pass-through":
-            common_inset += "\nget_active_solution().args = parser.parse_args()\n"
+            execution_block += "\nget_active_solution().args = parser.parse_args()\n"
 
-        self.common_inset = common_inset
+        execution_block += super().get_execution_block(solution_object)
+        execution_block += "\nget_active_solution().test()\n"
 
-    def set_execution_block(self, solution_object):
-        super().set_execution_block(solution_object)
-        self.execution_block += "\nget_active_solution().test()\n"
+        return execution_block
